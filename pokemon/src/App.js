5c5e4b742708ff3be9f6482f3cfcc6b6e820d0cd
Kuznetsov Maxim <m.kuznecov@z-it.ru>
2021-09-09 12:31:28 +0300

    hw-inline

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+import bg1 from "../src/assets/bg1.jpeg"
+import bg3 from "../src/assets/bg3.jpeg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Header title='This is title' descr='This is Description!'/>
+          <Layout title='Title1' descr='Descr1' urlBg={bg1} colorBg={null} />
+          <Layout title='Title2' descr='Descr2' urlBg={null} colorBg='#e2e2e2'/>
+          <Layout title='Title3' descr='Descr3' urlBg={bg3} colorBg={null} />
+          <Footer/>
+      </>
   );
 }
 
